51bd21a977c4c7eaa7f6e7f696f4b407df38b01c
RedPowDan <11051969dahul@mail.ru>
2021-01-25 15:58:22 +0300

    Начало...

---
+++ configs/config.py
@@ -0,0 +1,12 @@
+from transport.sanic.config import SanicConfig
+from db.config import SQLiteConfig, PostgresConfig
+
+
+class ApplicationConfig:
+    sanic: SanicConfig
+    database: PostgresConfig
+
+    def __init__(self):
+        self.sanic = SanicConfig()
+        # self.database = SQLiteConfig()
+        self.database = PostgresConfig()
